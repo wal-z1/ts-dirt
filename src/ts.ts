e87bc2ea5caac0905bd5c0a9c6a86586660e4f7d
Walid <ow_bouhenika@esi.dz>
2025-12-24 19:59:31 +0100

    commit 1

---
+++ src/ts.ts
@@ -0,0 +1,83 @@
+console.log("This is a TypeScript file.");
+console.log(Math.floor(4.7));
+
+//something
+
+let integervalue: number = 10;
+console.log(integervalue);
+
+let randomstring: string = "Hello";
+console.log(randomstring);
+
+let randomvalue: any = 5;
+
+console.log(randomvalue);
+
+type person = {
+	name: string;
+	age: number;
+	phone?: string; // optional property (can be undefined)
+};
+
+let walid: person = {
+	name: "Walid",
+	age: 20,
+	phone: "123-456-7890",
+};
+
+class PersonClass {
+	name: string;
+	age: number;
+
+	constructor(name: string, age: number) {
+		this.name = name;
+		this.age = age;
+	}
+}
+
+let person1 = new PersonClass("Alice", 30);
+
+function sayhello(person: person): string {
+	return `Hello, ${person.name}! You are ${person.age} years old.`;
+}
+
+function withoptional(num1: number, num2?: number) {
+	if (num2 !== undefined) {
+		return num1 + num2;
+	} else {
+		return num1;
+	}
+}
+
+function withdefault(num1: number, num2: number = 6) {
+	return num1 + num2;
+}
+
+function takeabunchofargs(...numbers: number[]) {
+	for (let num of numbers) {
+		console.log(num);
+	}
+}
+
+const adder = (a: number, b: number) => a + b;
+
+console.log(adder(5, 10));
+
+const voidfunction = (): void => {
+	console.log("void func");
+};
+
+// lets define a  mulitype variable
+let multivar: number | string;
+
+multivar = 5;
+
+console.log(multivar);
+multivar = "Now I'm a string";
+console.log(multivar);
+
+let onlythesevalues: 1 | 2 | 3;
+
+function onlytakethefollwoignvalues(val: 24 | 28 | 32) {
+	console.log(`passed in ${val}`);
+}
---
+++ ts.js
@@ -0,0 +1,2 @@
+console.log("This is a TypeScript file.");
+console.log(Math.floor(4.7));
